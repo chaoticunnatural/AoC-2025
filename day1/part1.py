e2647d6ef33dd6157fef3051dc2a2f03620573e6
yahaha5788 <j.thomps1310@gmail.com>
2025-12-02 14:42:19 -0500

    comments

--- day1/part1.py
+++ day1/part1.py
@@ -1,16 +1,16 @@
-with open('input.txt', 'r') as f:
-    rotations: list[str] = f.read().splitlines()
+with open('input.txt', 'r') as rotations_f:
+    rotations: list[str] = rotations_f.read().splitlines() # split into list like ['L29', 'R45', etc.]
 
     password: int = 0
-    current_loc: int = 50
+    current_loc: int = 50 # starts at 50 (i definitely didn't think my code was wrong for 10 minutes because i missed this)
 
     for rotation in rotations:
-        dir_mult: int = -1 if rotation[0] == "L" else 1
+        dir_mult: int = -1 if rotation[0] == "L" else 1 # negative if going left
         steps: int = int(rotation[1:])
 
-        current_loc += steps * dir_mult
+        current_loc += steps * dir_mult # make steps negative if left (dir_mult = -1)
 
-        if current_loc % 100 == 0:
+        if current_loc % 100 == 0: # check if the remainder if dividing location by 100 is zero, instead of having the number wrap around
             password += 1
 
-    print("password: " + str(password))
+    print(password)
--- day1/part2.py
+++ day1/part2.py
@@ -1,17 +1,17 @@
-with open('input.txt', 'r') as f:
-    rotations: list[str] = f.read().splitlines()
+with open('input.txt', 'r') as rotations_f:
+    rotations: list[str] = rotations_f.read().splitlines() # split into list like ['L29', 'R45', etc.]
 
     password: int = 0
-    current_loc: int = 50
+    current_loc: int = 50 # starts at 50 (i definitely didn't think my code was wrong for 10 minutes because i missed this)
 
     for rotation in rotations:
-        dir_inc: int = -1 if rotation[0] == "L" else 1
-        steps: int = int(rotation[1:])
+        dir_inc: int = -1 if rotation[0] == "L" else 1 # make direction negative if left because going it's going backwards
+        steps: int = int(rotation[1:]) # all chars in the string after 0 (the letter)
 
         for s in range(0, steps):
-            current_loc += dir_inc
+            current_loc += dir_inc # step once
 
             if current_loc % 100 == 0:
                 password += 1
 
-    print("password: " + str(password))
+    print(password)
